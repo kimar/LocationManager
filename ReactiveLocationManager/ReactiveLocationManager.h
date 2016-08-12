0936b42ecd2c0578fd210336c1c0f7f7aff8e98a
Marcus Kida <marcus@kida.io>
2016-08-13 09:56:35 +1000

    chore(header): remove dead comments

--- ReactiveLocationManager/ReactiveLocationManager.h
+++ ReactiveLocationManager/ReactiveLocationManager.h
@@ -1,11 +1,3 @@
-//
-//  ReactiveLocationManager.h
-//  ReactiveLocationManager
-//
-//  Created by Marcus Kida on 13/08/2016.
-//  Copyright © 2016 Marcus Kida. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 //! Project version number for ReactiveLocationManager.
@@ -13,7 +5,3 @@ FOUNDATION_EXPORT double ReactiveLocationManagerVersionNumber;
 
 //! Project version string for ReactiveLocationManager.
 FOUNDATION_EXPORT const unsigned char ReactiveLocationManagerVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <ReactiveLocationManager/PublicHeader.h>
-
-
